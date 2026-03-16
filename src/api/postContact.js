245762ddf6bb159c1c5c0622c31287fadd94582f
Pavlo Kuzmich <pkuzmich.cz@gmail.com>
2026-03-17 00:04:07 +0100

    Add Contact route and using TanStack mutation to submit contact form

---
+++ src/api/postContact.js
@@ -0,0 +1,15 @@
+export async function postContact({ name, email, message }) {
+  const response = await fetch("/api/contact", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ name, email, message }),
+  });
+
+  if (!response.ok) {
+    throw new Error("Failed to post contact");
+  }
+
+  return response.json();
+}
---
+++ src/routes/contact.lazy.jsx
@@ -0,0 +1,39 @@
+import { useMutation } from "@tanstack/react-query";
+import { createLazyFileRoute } from "@tanstack/react-router";
+import { postContact } from "../api/postContact";
+
+export const Route = createLazyFileRoute("/contact")({
+  component: ContactRoute,
+});
+
+function ContactRoute() {
+  const mutation = useMutation({
+    mutationFn: function (e) {
+      e.preventDefault();
+      const formData = new FormData(e.target);
+      const formProps = Object.fromEntries(formData);
+
+      return postContact({
+        name: formProps.name,
+        email: formProps.email,
+        message: formProps.message,
+      });
+    },
+  });
+
+  return (
+    <div className="contact">
+      <h2>Contact</h2>
+      {mutation.isSuccess ? (
+        <h3>Thank you for your message!</h3>
+      ) : (
+        <form onSubmit={mutation.mutate}>
+          <input type="text" name="name" placeholder="Name" />
+          <input type="email" name="email" placeholder="E-mail" />
+          <textarea name="message" placeholder="Message"></textarea>
+          <button>Submit</button>
+        </form>
+      )}
+    </div>
+  );
+}
